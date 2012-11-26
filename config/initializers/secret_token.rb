f56c8abb45659891e2911af08bf9615b66b7f2cc
emilien <emilien.charbonnier@free.fr>
2012-11-26 07:16:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sante::Application.config.secret_token = '56d3f00d1a86a4dc3ff10e6ccf3ada3fae3400a6bbcc0c2553e70e968e32b5cb93fa4ae6e6278371a0ade42155f2e5336461441f15b54b03cd9a162447164a7b'
